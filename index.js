9cba740c8d424ff63184f0e99a6976f5e7293ec4
Muhammad Abid Dewantoro <151120880+DeadEye27@users.noreply.github.com>
2026-03-17 13:46:11 +0700

    Finish the index.js Configuration
    Import the necessary Configuration

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const sequelize = require('./config/database');
+const noteRoutes = require('./routes/noteRoutes');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors({
+    origin: ['http://localhost', 'http://localhost:5173', 'http://127.0.0.1:5500'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials:true,
+}));
+
+app.use(express.json());
 
 app.get("/", (req, res) =>{
     res.send("Hello World!");
 });
 
+require('./schemas/Note');
+app.use('/notes', noteRoutes);
+
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+sequelize.sync().then(() => {
+    console.log('Database Synced');
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+});
