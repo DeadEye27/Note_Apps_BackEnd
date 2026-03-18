022132a4468613557b2d4a14a0709bc52503069a
Muhammad Abid Dewantoro <151120880+DeadEye27@users.noreply.github.com>
2026-03-18 17:40:37 +0700

    Add the alternative url http://127.0.0.1:5501
    The url is user for the second test page

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 // app.use(bodyParser.json());
 
 app.use(cors({
-    origin: ['http://localhost', 'http://localhost:5173', 'http://127.0.0.1:5500'],
+    origin: ['http://localhost', 'http://localhost:5173', 'http://127.0.0.1:5500', 'http://127.0.0.1:5501'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials:true,
 }));
